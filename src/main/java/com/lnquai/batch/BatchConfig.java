457224a6feb81844c60a764644f9e4a9ad1a351c
LELO <nyaka.lelo@econocom.com>
2020-11-09 05:11:46 +0100

    lecture des formateurs

--- src/main/java/com/lnquai/batch/BatchConfig.java
+++ src/main/java/com/lnquai/batch/BatchConfig.java
@@ -2,10 +2,19 @@ package com.lnquai.batch;
 
 import java.util.Arrays;
 
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,4 +42,22 @@ public class BatchConfig {
 		bean.setValidators(Arrays.asList(defaultJobParametersValidator(), myJobParametersValidator()));
 		return bean;
 	}
+
+	@Bean
+	public Step step1(StepBuilderFactory stepBuilderFactory) {
+		return stepBuilderFactory.get("step1").tasklet(new Tasklet() {
+			@Override
+			public RepeatStatus execute(StepContribution arg0, ChunkContext arg1) throws Exception {
+				System.out.println("Hello ");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	}
+
+	@Bean
+	public Job job(final JobBuilderFactory jobBuilderFactory, final Step chargementFormateursStep) {
+		return jobBuilderFactory.get("formations-batch").start(chargementFormateursStep)
+				.validator(compositeJobParametersValidator()).incrementer(new RunIdIncrementer()).build();
+	}
+
 }
--- src/main/java/com/lnquai/batch/ChargementFormateursStepConfig.java
+++ src/main/java/com/lnquai/batch/ChargementFormateursStepConfig.java
@@ -1,15 +1,38 @@
 package com.lnquai.batch;
 
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
 import com.lnquai.batch.domaine.Formateur;
 
+//Classe de configuration pour la step de chargement des formateurs
+@Configuration
 public class ChargementFormateursStepConfig {
+	// Implementation du bean ItemReader avec type lu Formateur.Pour lire la liste
+	// de formateurs
+	// il faut accéder au fichier grâce à l'objet Resource.On va utiliser le
+	// jobParameters pour accéder au chemin de ce fichier là.
+	// Spring va récupérer le chemin vers ce fichier via le jobParameter et va
+	// instancier un bean de type Resource et le mettre à disposition de cette
+	// méthode
+	// On va créer un flatfileitemreader en utilisant un builder.
+	// On commence d'abord par définir son nom, puis définir d'où il lira les lignes
+	// liées au formateur
+	// On va lire le fichier CSV delimitée par des points ; donc on va le dire à
+	// Spring
+	// Puis les noms de colonnes,la classe cible puis le build
+	// Attention , tout bean qui doit accéder au jobParameter doit être annotée par
+	// StepScope. Le bean ne sera créé qu'à l'intérieur de la scope et à ce moment
+	// là le jobParamater est mise
+	// à disposition du bean pour sa construction
 	@Bean
 	@StepScope
 	public FlatFileItemReader<Formateur> formateurItemReader(
@@ -17,6 +40,18 @@ public class ChargementFormateursStepConfig {
 		return new FlatFileItemReaderBuilder<Formateur>().name("FormateurItemReader").resource(inputFile).delimited()
 				.delimiter(";").names(new String[] { "id", "nom", "prenom", "adresseEmail" })
 				.targetType(Formateur.class).build();
+	}
+
+	@Bean
+	public ItemWriter<Formateur> formateurItemWriter() {
+		return (items) -> items.forEach(System.out::println);
+	}
+
+	// Une fois ce step créé, il faut la déclarer dans le job
+	@Bean
+	public Step chargementFormateursStep(final StepBuilderFactory builderFactory) {
+		return builderFactory.get("chargementFormateursStep").<Formateur, Formateur>chunk(10)
+				.reader(formateurItemReader(null)).writer(formateurItemWriter()).build();
 	}
 
 }
